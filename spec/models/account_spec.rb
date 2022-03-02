d0a920625ecc95f44821d243b897002412f7cffc
isaacboucinha <isaacboucinha@gmail.com>
2022-03-02 20:20:28 +0000

    minor refactoring on tests

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -6,57 +6,59 @@ require 'rails_helper'
 
 RSpec.describe Account, type: :model do
   describe 'model logic' do
-    it 'should not save account without name' do
-      account = Account.new
-      expect(account.save).to be_falsy
-    end
+    context 'on create' do
+      it 'should not save account without name' do
+        account = Account.new
+        expect(account.save).to be_falsy
+      end
 
-    it 'should not save account without lower case letters in name' do
-      account = Account.new(name: 'TEST123')
-      expect(account.save).to be_falsy
-    end
+      it 'should not save account without lower case letters in name' do
+        account = Account.new(name: 'TEST123')
+        expect(account.save).to be_falsy
+      end
 
-    it 'should not save account without upper case letters in name' do
-      account = Account.new(name: 'test123')
-      expect(account.save).to be_falsy
-    end
+      it 'should not save account without upper case letters in name' do
+        account = Account.new(name: 'test123')
+        expect(account.save).to be_falsy
+      end
 
-    it 'should not save account without digits in name' do
-      account = Account.new(name: 'Testtest')
-      expect(account.save).to be_falsy
-    end
+      it 'should not save account without digits in name' do
+        account = Account.new(name: 'Testtest')
+        expect(account.save).to be_falsy
+      end
 
-    it 'should not save account with less than 5 chars' do
-      # less than 5 characters
-      account = Account.new(name: 'Tes1')
-      expect(account.save).to be_falsy
-      account = Account.new(name: 'Test1')
-      expect(account.save).to be_truthy
-    end
+      it 'should not save account with less than 5 chars' do
+        # less than 5 characters
+        account = Account.new(name: 'Tes1')
+        expect(account.save).to be_falsy
+        account = Account.new(name: 'Test1')
+        expect(account.save).to be_truthy
+      end
 
-    it 'should not save account with more than 15 chars' do
-      # more than 15 characters
-      account = Account.new(name: 'Thisismorethan15characters')
-      expect(account.save).to be_falsy
-      account = Account.new(name: 'Exaactly15chars')
-      expect(account.save).to be_truthy
-    end
+      it 'should not save account with more than 15 chars' do
+        # more than 15 characters
+        account = Account.new(name: 'Thisismorethan15characters')
+        expect(account.save).to be_falsy
+        account = Account.new(name: 'Exaactly15chars')
+        expect(account.save).to be_truthy
+      end
 
-    it 'should save account with valid name' do
-      account = Account.new(name: 'Test123')
-      expect(account.save).to be_truthy
-    end
+      it 'should save account with valid name' do
+        account = Account.new(name: 'Test123')
+        expect(account.save).to be_truthy
+      end
 
-    it 'should not allow accounts with same name' do
-      account = Account.new(name: 'Test123')
-      account.save!
+      it 'should not allow accounts with same name' do
+        account = Account.new(name: 'Test123')
+        account.save!
 
-      another_account = Account.new(name: 'Test123')
-      expect(another_account.save).to be_falsy
+        another_account = Account.new(name: 'Test123')
+        expect(another_account.save).to be_falsy
 
-      # test case insensitivity
-      another_account = Account.new(name: 'TesT123')
-      expect(another_account.save).to be_falsy
+        # test case insensitivity
+        another_account = Account.new(name: 'TesT123')
+        expect(another_account.save).to be_falsy
+      end
     end
 
     context 'associations' do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,48 +6,50 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'model logic' do
-    it 'should not save user without name' do
-      user = User.new
-      expect(user.save).to be_falsy
-    end
+    context 'on create' do
+      it 'should not save user without name' do
+        user = User.new
+        expect(user.save).to be_falsy
+      end
 
-    it 'should not save user without password' do
-      user = User.new(name: 'test')
-      expect(user.save).to be_falsy
-    end
+      it 'should not save user without password' do
+        user = User.new(name: 'test')
+        expect(user.save).to be_falsy
+      end
 
-    it 'should save user with valid fields' do
-      user = User.new(name: 'test', password: 'test')
-      expect(user.save).to be_truthy
-    end
+      it 'should save user with valid fields' do
+        user = User.new(name: 'test', password: 'test')
+        expect(user.save).to be_truthy
+      end
 
-    it 'should hash user name on save' do
-      user = User.create!(name: 'test', password: 'test')
-      user.reload
-      expect(user.name).to eq(Digest::MD5.hexdigest('test'))
-    end
+      it 'should hash user name on save' do
+        user = User.create!(name: 'test', password: 'test')
+        user.reload
+        expect(user.name).to eq(Digest::MD5.hexdigest('test'))
+      end
 
-    it 'should encrypt password' do
-      user = User.create!(name: 'test', password: 'test')
-      expect(user.password_digest).not_to eq('test')
-      expect(user.authenticate('test')).to be_truthy
-    end
+      it 'should encrypt password' do
+        user = User.create!(name: 'test', password: 'test')
+        expect(user.password_digest).not_to eq('test')
+        expect(user.authenticate('test')).to be_truthy
+      end
 
-    it 'should not allow users with same name' do
-      User.create!(name: 'test', password: 'test')
-      another_user = User.new(name: 'test', password: 'another_test_password')
-      expect(another_user.save).to be_falsy
-    end
+      it 'should not allow users with same name' do
+        User.create!(name: 'test', password: 'test')
+        another_user = User.new(name: 'test', password: 'another_test_password')
+        expect(another_user.save).to be_falsy
+      end
 
-    it 'should allow users with same password, stored as different digests' do
-      user = User.create!(name: 'test', password: 'test')
-      another_user = User.new(name: 'another_test_password', password: 'test')
-      expect(another_user.save!).to be_truthy
+      it 'should allow users with same password, stored as different digests' do
+        user = User.create!(name: 'test', password: 'test')
+        another_user = User.new(name: 'another_test_password', password: 'test')
+        expect(another_user.save!).to be_truthy
 
-      user.reload
-      another_user.reload
+        user.reload
+        another_user.reload
 
-      expect(user.password_digest).not_to eq(another_user.password_digest)
+        expect(user.password_digest).not_to eq(another_user.password_digest)
+      end
     end
 
     context 'associations' do
